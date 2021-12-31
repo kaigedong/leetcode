8a6cb11d8d998e0a9ee0fe428739fd45d4ed317d
kaigedong <dongkaige@gmail.com>
2021-12-31 19:09:56 +0800

    回文数字解答

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod q_1_two_sum;
 mod q_2_add_two_num;
+mod q_9_palindrome_number;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/q_9_palindrome_number.rs
@@ -0,0 +1,34 @@
+use std::string;
+
+struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        if x < 10 {
+            return true;
+        }
+
+        let s = x.to_string();
+        if s.len() % 2 == 1 {
+            s[0..(s.len() / 2)].to_string()
+                == s[(s.len() / 2 + 1)..].chars().rev().collect::<String>()
+        } else {
+            s[0..(s.len() / 2)].to_string() == s[(s.len() / 2)..].chars().rev().collect::<String>()
+        }
+    }
+}
+
+#[test]
+fn test_palindrome() {
+    assert_eq!(Solution::is_palindrome(121), true);
+    assert_eq!(Solution::is_palindrome(-121), false);
+    assert_eq!(Solution::is_palindrome(1), true);
+    assert_eq!(Solution::is_palindrome(-1), false);
+    assert_eq!(Solution::is_palindrome(10), false);
+    assert_eq!(Solution::is_palindrome(-101), false);
+    assert_eq!(Solution::is_palindrome(11), true);
+    assert_eq!(Solution::is_palindrome(1001), true);
+}
